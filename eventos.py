6fc94d43f1edced259f71b8ef6689234fc814d5c
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2025-09-28 20:51:48 -0300

    Update eventos.py

--- eventos.py
+++ eventos.py
@@ -42,18 +42,18 @@ DATAS_PAGAMENTO_VA_VR = {
 }
 
 DATAS_LIMITE_BENEFICIOS = {
-    datetime.date(2025, 1, 10): "data limite para solicitações e alterações de benefícios (Janeiro)",
-    datetime.date(2025, 2, 10): "data limite para solicitações e alterações de benefícios (Fevereiro)",
-    datetime.date(2025, 3, 11): "data limite para solicitações e alterações de benefícios (Março)",
-    datetime.date(2025, 4, 10): "data limite para solicitações e alterações de benefícios (Abril)",
-    datetime.date(2025, 5, 12): "data limite para solicitações e alterações de benefícios (Maio)",
-    datetime.date(2025, 6, 10): "data limite para solicitações e alterações de benefícios (Junho)",
-    datetime.date(2025, 7, 10): "data limite para solicitações e alterações de benefícios (Julho)",
-    datetime.date(2025, 8, 11): "data limite para solicitações e alterações de benefícios (Agosto)",
-    datetime.date(2025, 9, 10): "data limite para solicitações e alterações de benefícios (Setembro)",
-    datetime.date(2025, 10, 10): "data limite para solicitações e alterações de benefícios (Outubro)",
-    datetime.date(2025, 11, 10): "data limite para solicitações e alterações de benefícios (Novembro)",
-    datetime.date(2025, 12, 10): "data limite para solicitações e alterações de benefícios (Dezembro)",
+    datetime.date(2025, 1, 10): "data limite de solicitações e alterações de benefícios (Janeiro)",
+    datetime.date(2025, 2, 10): "data limite de solicitações e alterações de benefícios (Fevereiro)",
+    datetime.date(2025, 3, 11): "data limite de solicitações e alterações de benefícios (Março)",
+    datetime.date(2025, 4, 10): "data limite de solicitações e alterações de benefícios (Abril)",
+    datetime.date(2025, 5, 12): "data limite de solicitações e alterações de benefícios (Maio)",
+    datetime.date(2025, 6, 10): "data limite de solicitações e alterações de benefícios (Junho)",
+    datetime.date(2025, 7, 10): "data limite de solicitações e alterações de benefícios (Julho)",
+    datetime.date(2025, 8, 11): "data limite de solicitações e alterações de benefícios (Agosto)",
+    datetime.date(2025, 9, 10): "data limite de solicitações e alterações de benefícios (Setembro)",
+    datetime.date(2025, 10, 10): "data limite de solicitações e alterações de benefícios (Outubro)",
+    datetime.date(2025, 11, 10): "data limite de solicitações e alterações de benefícios (Novembro)",
+    datetime.date(2025, 12, 10): "data limite de solicitações e alterações de benefícios (Dezembro)",
 }
 
 DATAS_PAGAMENTO_SALARIO = {
