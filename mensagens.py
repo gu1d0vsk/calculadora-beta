862b7dcba203bb3efa6513c85be46b0d0b9a37b7
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2025-09-28 16:30:46 -0300

    Create mensagens.py

---
+++ mensagens.py
@@ -0,0 +1,15 @@
+import datetime
+
+def obter_mensagem_do_dia():
+    """Retorna uma mensagem engraçada baseada no dia da semana."""
+    hoje = datetime.date.today().weekday() # Segunda é 0 e Domingo é 6
+    mensagens = {
+        0: "Segundou, EBA!'",
+        1: "Terça-feira 📌",
+        2: "Quarta-feira, semana praticamente encerrada.",
+        3: "Quinta-feira 📆",
+        4: "Enfim Sextou",
+        5: "Sábado 😴",
+        6: "Domingo 💤"
+    }
+    return mensagens.get(hoje, "Calculadora de Jornada")
