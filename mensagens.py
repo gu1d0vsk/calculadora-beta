89fd401351b5013b63b9da8c49f4b9170e523cd0
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2025-09-29 11:58:05 -0300

    Update mensagens.py

--- mensagens.py
+++ mensagens.py
@@ -4,7 +4,7 @@ def obter_mensagem_do_dia():
     """Retorna uma mensagem engraçada baseada no dia da semana."""
     hoje = datetime.date.today().weekday() # Segunda é 0 e Domingo é 6
     mensagens = {
-        0: "Segundou, EBA!",
+        0: "Segundou, EBA!!!",
         1: "Terça-feira 📌",
         2: "Quarta-feira, semana praticamente encerrada.",
         3: "Quinta-feira 📆",
