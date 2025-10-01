ef6d8fd1e84e65f35807d2d01e31d59c07b31876
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2025-10-01 14:04:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -327,8 +327,8 @@ st.markdown("""
     .st-at {    border-top-left-radius: 1.5rem;}
     .st-emotion-cache-yinll1 svg { display: none; } 
     .st-emotion-cache-ubko3j svg { display: none; }
-    ._profileImage_gzau3_78._darkThemeShadow_gzau3_91 {  display: none;}
-._container_gzau3_1._viewerBadge_nim44_23 {  display: none;}
+    ._profileImage_gzau3_78._darkThemeShadow_gzau3_91 {  display: none !important;}
+._container_gzau3_1._viewerBadge_nim44_23 {  display: none !important;}
 
 </style>
 """, unsafe_allow_html=True)
