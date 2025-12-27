535f4371e9d8c09dc983677c07dbfc1cf8bdaf04
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2025-12-27 09:34:29 -0300

    Update CSS styles to hide elements in app.py

--- app.py
+++ app.py
@@ -262,8 +262,16 @@ st.markdown(f"""
         pointer-events: none !important;
         display: none !important;
     }}
-    ._container_gzau3_1._viewerBadge_nim44_23 {{  display: none !important;}}
+    ._container_gzau3_1. {{  display: none !important;}}
+    
     ._profileContainer_gzau3_53 {{  display: none !important;}}
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
     /* 2. Oculta o Rodapé Padrão */
     footer, [data-testid="stFooter"] {{
