83205ad2d0137fac4f492f6e899a680abc36cd73
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2025-12-31 08:55:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -251,12 +251,14 @@ else:
     """
 
 st.markdown(f"""
-<style>
 [data-testid="stApp"] {{
         background: linear-gradient(rgb(2, 45, 44) 0%, rgb(0, 21, 21) 100%) !important;
         background-attachment: fixed !important;
         background-size: cover !important;
     }}
+    
+<style>
+
     /* --- CSS "NUCLEAR" PARA LIMPAR A INTERFACE DO STREAMLIT --- */
     
     /* Esconde o rodapé padrão "Made with Streamlit" */
