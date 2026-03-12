b444c4fa27ae302c23764575cb95c4db176e5184
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2026-03-12 15:58:10 -0300

    Adjust padding and font size in toggle styles

--- app.py
+++ app.py
@@ -354,7 +354,7 @@ st.markdown(f"""
     div[data-testid="stToggle"] {{
         
         margin-top: -20px !important;           /* Puxa mais pra perto do botão calcular */
-        padding-left: 2000px;
+        padding-left: 2px;
         opacity: 0.5 !important;   /* Aplicação direta do Python na força bruta */
         transition: opacity 0.2s ease-in-out;
     }}
@@ -362,7 +362,7 @@ st.markdown(f"""
         opacity: 1.0 !important;                /* Acende se passar o dedo/mouse por cima */
     }}
     div[data-testid="stToggle"] label p {{
-        font-size: 0.9rem !important;
+        font-size: 0.6rem !important;
         color: #e0e0e0 !important;
     }}
     /* -------------------------------------- */
